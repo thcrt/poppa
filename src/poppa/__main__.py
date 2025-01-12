f4b1bb484c7bc4ccd7d6e7f7d7b6eadcb248713b
thcrt <110127860+thcrt@users.noreply.github.com>
2025-01-12 16:55:06 +0100

    Add prettier verbose output

--- src/poppa/__main__.py
+++ src/poppa/__main__.py
@@ -2,15 +2,15 @@ from enum import StrEnum, auto
 from pathlib import Path
 
 import pyexcel  # type: ignore
-import rich
-import rich.table
+from rich.table import Table
+from rich.console import Console
 import typer
 
 from .errors import ErrorManager
 
 app = typer.Typer(pretty_exceptions_enable=False)
-stdout = rich.console.Console()
-stderr = rich.console.Console(stderr=True)
+stdout = Console()
+stderr = Console(stderr=True)
 error_manager = ErrorManager(Path("./choices.poppa.csv"), stdout, stderr)
 
 
@@ -40,13 +40,63 @@ def parse(
     people = build_people(data, places_manager)
     families = build_families(people)
 
+    people_table = Table(
+        "ID",
+        "First",
+        "Last",
+        "Birth date",
+        "Birth place",
+        "Death date",
+        "Death place",
+        title="People",
+    )
+    for person in people.values():
+        people_table.add_row(
+            str(person.id_number),
+            person.first,
+            person.last,
+            person.birth_date,
+            person.birth_place,
+            person.death_date,
+            person.death_place,
+        )
+    stdout.print(people_table)
+
+    families_table = Table(
+        "Partner 1",
+        "Partner 2",
+        "Married date",
+        "Married place",
+        "Children",
+        title="Families",
+    )
+    for family in families:
+        families_table.add_row(
+            str(family.partner1.id_number) if family.partner1 else "",
+            str(family.partner2.id_number) if family.partner2 else "",
+            family.married_date,
+            family.married_place,
+            ", ".join(str(child.id_number) for child in family.children),
+        )
+    stdout.print(families_table)
+
     with out.open("w+") as f:
         written = export(f, people, families, places_manager)
 
-    print("Done exporting.")
-    for label, stat in written.items():
-        print(f"  - {label.title()}: {stat}")
+    stdout.rule(style="bold white")
+    stdout.rule(style="bold white")
 
+    export_table = Table(
+        "Kind",
+        "Quantity",
+        title="Records exported"
+    )
+    for record_type, number_exported in written.items():
+        export_table.add_row(
+            record_type.title(),
+            str(number_exported)
+        )
+    stdout.print(export_table)
 
 if __name__ == "__main__":
     app()
